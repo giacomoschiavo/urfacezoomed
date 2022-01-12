642cfb4af08e2a428c5071e60eb735392bbe45f9
giacomoschiavo <giacomo.schiavo12@gmail.com>
2022-01-12 21:49:02 +0100

    thankfully the last commit, added style and made some arrangements. Sorry but now I have to go

--- src/index.js
+++ src/index.js
@@ -1,80 +1,55 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 import * as tf from "@tensorflow/tfjs";
-import { CLASSES } from "./labels";
+import { startIndicesWithElidedDims } from "@tensorflow/tfjs-core/dist/ops/slice_util";
 
 const blazeface = require("@tensorflow-models/blazeface");
+
 async function doStuff() {
   try {
     const model = await blazeface.load();
-    const mysteryVideo = document.getElementById("mystery");
-    const camDetails = await setupWebcam(mysteryVideo); // returns [ctx, imgHeight, imgWidth]
-    main(model, mysteryVideo, camDetails);
+    const hiddenVideo = document.getElementById("hiddenVideo");
+    const camDetails = await setupWebcam(hiddenVideo); // returns [ctx, imgHeight, imgWidth]
+    main(model, hiddenVideo, camDetails);
   } catch (error) {
-    console.log(error);
+    Swal.fire({
+      title: "Woooops",
+      text: "Sorry but I need to see your face🧐",
+      icon: "error",
+      confirmButtonText: "Ok",
+    });
   }
 }
 
-async function main(model, mysteryVideo, camDetails) {
+async function main(model, hiddenVideo, camDetails) {
   const [ctx] = camDetails;
-  const predictions = await model.estimateFaces(mysteryVideo, false);
-
+  const predictions = await model.estimateFaces(hiddenVideo, false);
   if (predictions.length > 0) {
-    /*
-    [
-      {
-        topLeft: [232.28, 145.26],
-        bottomRight: [449.75, 308.36],
-        probability: [0.998],
-        landmarks: [
-          [295.13, 177.64], // right eye
-          [382.32, 175.56], // left eye
-          [341.18, 205.03], // nose
-          [345.12, 250.61], // mouth
-          [252.76, 211.37], // right ear
-          [431.20, 204.93] // left ear
-        ]
-      }
-    ]
-    */
     predictions.forEach((prediction) => {
-      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+      if (prediction.probability < 0.9) return;
       const start = prediction.topLeft;
       const end = prediction.bottomRight;
       const size = [end[0] - start[0], end[1] - start[1]];
-      // ctx.drawImage(mysteryVideo, 0, 0, ctx.canvas.width, ctx.canvas.height);
-
-      // Render a rectangle over each detected face.
+      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
       ctx.drawImage(
-        mysteryVideo,
-        start[0],
-        start[1],
-        size[0],
-        size[1],
+        hiddenVideo,
+        parseInt(start[0], 0),
+        parseInt(start[1], 0),
+        parseInt(size[0], 0),
+        parseInt(size[1], 0),
         0,
         0,
         ctx.canvas.width,
         ctx.canvas.height
       );
-      // ctx.strokeRect(start[0], start[1], size[0], size[1]);
-
-      // const landmarks = prediction.landmarks;
-      // ctx.fillStyle = "blue";
-      // for (let j = 0; j < landmarks.length; j++) {
-      //   const x = landmarks[j][0];
-      //   const y = landmarks[j][1];
-      //   ctx.fillRect(x, y, 5, 5);
-      // }
     });
   } else {
-    ctx.drawImage(mysteryVideo, 0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.drawImage(hiddenVideo, 0, 0, ctx.canvas.width, ctx.canvas.height);
   }
 
-  // tf.dispose([model]);
-
-  console.log("Memory status:", tf.memory().numTensors);
+  // console.log("Memory status:", tf.memory().numTensors);
   requestAnimationFrame(() => {
-    main(model, mysteryVideo, camDetails);
+    main(model, hiddenVideo, camDetails);
   });
 }
 
@@ -94,7 +69,7 @@ async function setupWebcam(videoRef) {
 
     return new Promise((resolve, _) => {
       videoRef.onloadedmetadata = () => {
-        const detection = document.getElementById("detection");
+        const detection = document.getElementById("zoomedVideo");
         const ctx = detection.getContext("2d");
         const imgWidth = videoRef.clientWidth;
         const imgHeight = videoRef.clientHeight;
@@ -104,7 +79,12 @@ async function setupWebcam(videoRef) {
       };
     });
   } else {
-    alert("no webcam");
+    Swal.fire({
+      title: "Oh, sorry!",
+      text: "It seems like you have no webcam😥",
+      icon: "error",
+      confirmButtonText: "Alright",
+    });
   }
 }
 
